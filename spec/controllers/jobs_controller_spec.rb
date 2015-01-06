577ba716eedc86f13e10a72c13ae9639a0523165
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-05 19:16:54 -0800

    added all controller tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,19 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe JobsController, :type => :controller do
-  describe "A user can" do
-    it "visit the home page and see a list of jobs" do
-      visit '/'
-      expect(page).to have_content "All job listings"
+  context "#index" do
+    it "loads all jobs into @jobs" do
+      get :index
+      expect(assigns(:jobs)).to eq Job.all
     end
-    it "create a job" do
+  end
+
+  context "#new" do
+    it "assigns @job to Job.new" do
+      get :new
+      expect(assigns(:job)).to eq Job.new
+    end
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template "new"
+    end
+  end
+
+  context "#create" do
+    it "with valid params" do
+      expect {
+        post :create, :job => FactoryGirl.attributes_for(:job)
+        expect(response).to be_redirect
+      }.to change { Job.count }.by(1)
+    end
+
+    it "with invalid params" do
+      expect {
+        post :create, :job => ""
+        expect(response).to_not be_redirect
+      }.to_not change { Job.count }
+    end
+  end
+
+  context "#show" do
+    it "finds a job by id" do
       job = FactoryGirl.create(:job)
+      get :show, id: job.id
+      expect(assigns(:job)).to eq job
     end
-    it "edit a job" do
+  end
+
+  context "#edit" do
+     it "edits a job by id" do
+      job = FactoryGirl.create(:job)
+      get :edit, id: job.id
+      expect(assigns(:job)).to eq job
     end
-    it "update a job they created" do
+  end
+
+  context "#update" do
+    it "with valid params" do
+      job = FactoryGirl.create(:job)
+      patch :update, id: job.id, job: {title: "Front End Developer"}
+      expect(assigns(:job)).to eq job
     end
-    it "delete a job" do
+
+    it "with invalid params" do
+      job = FactoryGirl.create(:job)
+      patch :update, id: job.id, job: {tile: ""}
+       expect(response).to render_template "edit"
+    end
+  end
+
+  context "#destroy" do
+    it "assigns a job to Job.find(params[:id])" do
+      job = FactoryGirl.create(:job)
+      delete :destroy, id: job.id
+      expect(assigns(:job)).to eq job
+    end
+
+    it "destroys a poem" do
+      job = FactoryGirl.create(:job)
+      delete :destroy, id: job.id
+      expect(Job.all).to_not include job
+    end
+
+    it "redirects to the root_path when successfully destroyed" do
+       job = FactoryGirl.create(:job)
+       delete :destroy, id: job.id
+       expect(response).to be_redirect
     end
   end
 end
