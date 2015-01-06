b18aad60bd01c3faa2fa385ba92bba701a76346a
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-05 19:24:04 -0800

    #index tests pass

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,23 @@ class JobsController < ApplicationController
     @jobs = Job.all
     @job = Job.new
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
