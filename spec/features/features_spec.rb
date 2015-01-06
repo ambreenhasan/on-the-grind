7d00bc81497fba0d1c72fdb6e21235d8a547df45
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-05 23:13:20 -0800

    added a few feature tests

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "User visits the homepage" do
+  scenario 'and is able to see a list of all job listings' do
+    visit root_path
+    expect(page).to have_content "All job listings"
+  end
+  scenario 'and click a link to create a new job listing' do
+    visit root_path
+    click_on "Add job"
+  end
+end
+
+feature "User can create a new job listing" do
+  # let!(:job) {}
+  scenario "And fill out the form with valid params" do
+  end
+end
+
+feature "User can edit a job listing" do
+end
+
+feature "User can delete a job listing" do
+end
+
+feature "User can sign in" do
+  scenario "and add a job to their list" do
+  end
+  scenario "and see a list of jobs they have added" do
+  end
+end
